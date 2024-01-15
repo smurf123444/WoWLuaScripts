81011ffe5c0caff4129058b961761fc209874f42
Coder <smurf123444@gmail.com>
2024-01-15 10:27:41 -0800

    added

--- Mythic/MythicUI/MythicUIClient.lua
+++ Mythic/MythicUI/MythicUIClient.lua
@@ -3,7 +3,7 @@ if AIO.AddAddon() then
     return
 end
 
-local MyHandlers = AIO.AddHandlers("MythicUI", {})
+local MyHandlers = AIO.AddHandlers("MYTHIC_CLIENT", {})
 
 -- Attribute window
 local frameAttributes = CreateFrame("Frame", "frameAttributes", UIParent)
@@ -118,136 +118,9 @@ subFontAttributesInfo:SetPoint("TOPLEFT", -20, -45)
 
 
 
-local function CreateAttributesFrame(name, titleText)
-    local frame = CreateFrame("Frame", name, UIParent)
-    frame:SetSize(250, 300)
-    frame:SetMovable(true)
-    frame:EnableMouse(true)
-    frame:RegisterForDrag("LeftButton")
-    frame:SetPoint("CENTER")
-    frame:SetBackdrop({
-        bgFile = "Interface/AchievementFrame/UI-Achievement-Parchment-Horizontal",
-        edgeFile = "Interface/DialogFrame/UI-DialogBox-Border",
-        edgeSize = 20,
-        insets = { left = 5, right = 5, top = 5, bottom = 5 }
-    })
-    frame:SetScript("OnDragStart", frame.StartMoving)
-    frame:SetScript("OnHide", frame.StopMovingOrSizing)
-    frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
-    frame:Hide()
-
-    local titleBar = CreateFrame("Frame", name .. "TitleBar", frame, nil)
-    titleBar:SetSize(135, 25)
-    titleBar:SetBackdrop({
-        bgFile = "Interface/CHARACTERFRAME/UI-Party-Background",
-        edgeFile = "Interface/DialogFrame/UI-DialogBox-Border",
-        tile = true,
-        edgeSize = 16,
-        tileSize = 16,
-        insets = { left = 5, right = 5, top = 5, bottom = 5 }
-    })
-    titleBar:SetPoint("TOP", 0, 9)
-
-    local titleText1 = titleBar:CreateFontString(name .. "TitleText")
-    titleText1:SetFont("Fonts\\FRIZQT__.TTF", 13)
-    titleText1:SetSize(190, 5)
-    titleText1:SetPoint("CENTER", 0, 0)
-    titleText1:SetText("|cffFFC125" .. titleText .. "|r")
-
-    local infoText = frame:CreateFontString(name .. "Info")
-    infoText:SetFont("Fonts\\FRIZQT__.TTF", 15)
-    infoText:SetSize(300, 400)
-    infoText:SetPoint("TOPLEFT", -20, -45)
-
-    local closeButton = CreateFrame("Button", nil, frame, "UIPanelCloseButton")
-    closeButton:SetPoint("TOPRIGHT", -5, -5)
-    closeButton:EnableMouse(true)
-    closeButton:SetSize(27, 27)
-    closeButton:SetScript("OnClick", function()
-        frame:Hide()
-    end)
-
-    return frame, infoText
-end
-
-local frame1, frame1InfoText = CreateAttributesFrame("frame1InfoText", "frame1InfoText Info")
-local frame2, frame2InfoText = CreateAttributesFrame("frame2InfoText", "frame2InfoText Info")
-local frame3, frame3InfoText = CreateAttributesFrame("frame3InfoText", "frame3InfoText Info")
-local frame4, frame4InfoText = CreateAttributesFrame("frame4InfoText", "frame4InfoText Info")
-local frame5, frame5InfoText = CreateAttributesFrame("frame5InfoText", "frame5InfoText Info")
-local frame6, frame6InfoText = CreateAttributesFrame("frame6InfoText", "frame6InfoText Info")
-
-
-
-
-
-local function createButton(name, yOffset, onClick)
-    local button = CreateFrame("Button", name, frameAttributes, nil)
-    button:SetSize(20, 20)
-    button:SetPoint("TOPLEFT", 130, yOffset)
-    button:EnableMouse(true)
-    button:SetNormalTexture("Interface/BUTTONS/UI-SpellbookIcon-NextPage-Up")
-    button:SetHighlightTexture("Interface/BUTTONS/UI-Panel-MinimizeButton-Highlight")
-    button:SetPushedTexture("Interface/BUTTONS/UI-SpellbookIcon-NextPage-Down")
-    button:SetScript("OnMouseUp", onClick)
-end
-local function createButton2(name, yOffset, onClick)
-    local button = CreateFrame("Button", name, frameAttributes, nil)
-    button:SetSize(20, 20)
-    button:SetPoint("TOPLEFT", 260, yOffset)
-    button:EnableMouse(true)
-    button:SetNormalTexture("Interface/BUTTONS/UI-SpellbookIcon-NextPage-Up")
-    button:SetHighlightTexture("Interface/BUTTONS/UI-Panel-MinimizeButton-Highlight")
-    button:SetPushedTexture("Interface/BUTTONS/UI-SpellbookIcon-NextPage-Down")
-    button:SetScript("OnMouseUp", onClick)
-end
-
---[[ createButton("buttonAttributesIncreaseAttack", -70, function() ShowItem1() end)
-createButton("buttonAttributesIncreaseStrength", -120, function() ShowItem2() end)
-createButton("buttonAttributesIncreaseDefence", -160, function() ShowItem3() end)
-
-
-createButton2("buttonAttributesIncreaseCrafting", -70,  function() ShowItem4() end)
-createButton2("buttonAttributesIncreaseSmithing", -120, function() ShowItem5() end)
-createButton2("buttonAttributesIncreaseMining", -160, function() ShowItem6() end) ]]
-
--- Ranged skill
-function ShowItem1()
-    frame1InfoText:SetText("|cffFFC125frame1InfoText Skill Info|r")
-    frame1:Show()
-
-end
-
-function ShowItem2()
-    frame2InfoText:SetText("|cffFFC125frame2InfoText Skill Info|r")
-    frame2:Show()
-
-end
-
-function ShowItem3()
-    frame3InfoText:SetText("|cffFFC125frame3InfoText Skill Info|r")
-    frame3:Show()
-end
-
 function MyHandlers.ShowAttributes()
     frameAttributes:Show()
 end
-
-function ShowItem4()
-    frame4InfoText:SetText("|cffFFC125frame4InfoText Skill Info|r")
-    frame4:Show()
-end
-
-function ShowItem5()
-    frame5InfoText:SetText("|cffFFC125frame5InfoText Skill Info|r")
-    frame5:Show()
-end
-
-function ShowItem6()
-    frame6InfoText:SetText("|cffFFC125frame6InfoText Skill Info|r")
-    frame6:Show()
-end
-
 function MyHandlers.SetText1(player, text)
     fontAttributesTitleText:SetText("|cffFFC125Mythic Progress Summary|r")
     fontAttributesInfo1:SetText("|cFF000000" .. text .. "|r")
--- Mythic/MythicUI/MythicUIServer.lua
+++ Mythic/MythicUI/MythicUIServer.lua
@@ -1,7 +1,7 @@
 local AIO = AIO or require("AIO")
-local MyHandlers = AIO.AddHandlers("MythicUI", {})
+local MyHandlers = AIO.AddHandlers("MYTHIC_SERVER", {})
 
-local function AddPlayerStats(msg, player)
+function MyHandlers.UpdateMythicInfo(msg, player)
     local querySelect = "SELECT * FROM mythic_weekly_progress WHERE player_id = " .. player:GetGUIDLow()
     local result = CharDBQuery(querySelect)
     local statsTextLevel1 = ""
@@ -11,31 +11,31 @@ local function AddPlayerStats(msg, player)
             {name = "Last Mythic Level", levelIndex = 1, weekStartDateIndex=2,lastRewardID=3,},
         }
 
-        for i, level in ipairs(items) do
-            local level = result:GetUInt32(level.levelIndex)
-            local week = result:GetString(level.weekStartDateIndex)
-            local lastRewardID = result:GetString(level.lastRewardID)
+        for i, skill in ipairs(items) do
+            local level = result:GetUInt32(skill.levelIndex)
+            local week = result:GetString(skill.weekStartDateIndex)
+            local lastRewardID = result:GetString(skill.lastRewardID)
             local elapsedTime = (os.time(os.date("!*t")) - week)
 
-            statsTextLevel1 = statsTextLevel1 .. level.name .. " completed: " .. level .. "\n\n"
+            statsTextLevel1 = statsTextLevel1 .. skill.name .. " completed: " .. level .. "\n\n"
             statsTextLevel1 = statsTextLevel1 ..  " Week Start Date: " .. week .. "\n\n"
             statsTextLevel1 = statsTextLevel1 ..  " Last Reward ID: " .. lastRewardID .. "\n\n"
             statsTextLevel1 = statsTextLevel1 ..  " Elapsed Time: " .. elapsedTime .. "\n\n"
 
         end
     end
-    return msg:Add("MythicUI", "SetText1", statsTextLevel1)
+    return msg:Add("MYTHIC_CLIENT", "SetText1", statsTextLevel1)
 end
 
-AIO.AddOnInit(AddPlayerStats)
-
-for k,v in ipairs(GetPlayersInWorld()) do
-    OnLogin(3, v)
+local function UpdateMythic(player)
+    MyHandlers.UpdateMythicInfo(AIO.Msg(), player):Send(player)
 end
 
 local function AttributesOnCommand(event, player, command)
+    UpdateMythic(player)
     if(command == "para") then
-        AIO.Handle(player, "MythicUI", "ShowAttributes")
+
+        AIO.Handle(player, "MYTHIC_CLIENT", "ShowAttributes")
         return false
     end
 end
